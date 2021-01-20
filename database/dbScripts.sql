a7e427fcc8a25382635e1512288ccc4e3987289a
gurkitis <62221755+gurkitis@users.noreply.github.com>
2021-01-20 20:56:41 +0200

    fixed database script

--- database/dbScripts.sql
+++ database/dbScripts.sql
@@ -1,4 +1,10 @@
-create table items(
+DROP DATABASE IF EXISTS php_assignmentdb;
+
+CREATE DATABASE php_assignmentdb;
+
+USE php_assignmentdb;
+
+CREATE TABLE items(
 	SKU varchar(255) UNIQUE,
 	Product_Name varchar(255),
 	Price decimal(10, 2),
@@ -6,8 +12,5 @@ create table items(
 	Type_attribute varchar(255)
 );
 
-USE php_assignmentDB;
 INSERT INTO Items (SKU, Product_Name, Price, Product_type, Type_attribute) VALUES ('007', 'KAKISI', 420.69, 'DVD', '69MB');
 INSERT INTO Items (SKU, Product_Name, Price, Product_type, Type_attribute) VALUES ('666', 'Terminator', 332, 'Book', '1000KG');
-
-select * from Items;
